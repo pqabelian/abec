c270616a9093e79b3a009221d7ac0c8aa8f8fb5f
Team ABE <abeteam@outlook.com>
2020-06-24 21:03:52 +0800

    The definition of ABE and Neutrino

--- abeutil/const.go
+++ abeutil/const.go
@@ -10,3 +10,14 @@ const (
 	// MaxSatoshi is the maximum transaction amount allowed in satoshi.
 	MaxSatoshi = 21e6 * SatoshiPerBitcoin
 )
+
+const (
+	// SatoshiPerBitcent is the number of satoshi in one bitcoin cent.
+	//	SatoshiPerBitcent = 1e6
+
+	// NeutrinoPerAbe is the number of Neutrino in one Abe (1 ABE).
+	NeutrinoPerAbe = 1e7
+
+	// MaxNeutrino is the maximum transaction amount allowed in Neutrino.
+	MaxNeutrino = 296e6 * NeutrinoPerAbe
+)
