9d1ddca2ceca0a066328f0d1db8c117123f3d12f
depressi0n <793377875@qq.com>
2021-02-27 14:44:19 +0800

    overflow bug
    Fix a bug that incurs overflow of amount

--- abeutil/amount.go
+++ abeutil/amount.go
@@ -97,7 +97,7 @@ func NewAmountAbe(f float64) (Amount, error) {
 		return 0, errors.New("invalid ABE amount")
 	}
 
-	return round(f * NeutrinoPerAbe), nil
+	return round(f), nil
 }
 
 // ToUnit converts a monetary amount counted in bitcoin base units to a
