404f990df1f6e9b53100459fe11e672b9f50fdb5
alicebobscorpio <alicebobscorpio@gmail.com>
2022-09-19 09:01:13 +0800

    Update consensus_test.go
    add a testcase for VerifySealFast

--- consensus/ethash/consensus_test.go
+++ consensus/ethash/consensus_test.go
@@ -1,8 +1,11 @@
 package ethash
 
 import (
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"github.com/abesuite/abec/chainhash"
+	"golang.org/x/crypto/sha3"
 	"math/big"
 	"testing"
 	"time"
@@ -90,4 +93,33 @@ func TestHashToBig(t *testing.T) {
 	//if a.Cmp(c) == 0 {
 	//	fmt.Println("a equal c")
 	//}
+}
+
+func TestVerifySealFast(t *testing.T) {
+	contentHashBytes, _ := hex.DecodeString("fc08e94e72a8c97961a13848c26887755cc39820a7f3b8ad8bb3f7decbfe253f")
+	mixDigestBytes, _ := hex.DecodeString("718933ca9e97ce3b609e3249f23438268cdeecabb3087a080bed09c6429fd015")
+	nonce := uint64(1090297)
+	contentHash, _ := chainhash.NewHash(contentHashBytes)
+	mixDigest, _ := chainhash.NewHash(mixDigestBytes)
+
+	seedTmp := make([]byte, chainhash.HashSize+8)
+	copy(seedTmp, contentHash[:])
+	binary.LittleEndian.PutUint64(seedTmp[chainhash.HashSize:], nonce)
+
+	// we use the standard SHA3-512, rather than LegacyKeccak512
+	// seed = crypto.Keccak512(seed)
+	//seed := make([]byte, 64)
+	seed := sha3.Sum512(seedTmp)
+	//copy(seed, hashTmp[:])
+
+	sealHash := chainhash.ChainHash(append(seed[:], mixDigest[:]...))
+
+	//	Optimization: directly copy the codes of hashToBig() here, rather call the function hashToBig(),
+	//	since TrySeal() will be called in very frequently in mining.
+	//	This part codes must match with that in VerifySeal().
+	for i := 0; i < chainhash.HashSize/2; i++ {
+		sealHash[i], sealHash[chainhash.HashSize-1-i] = sealHash[chainhash.HashSize-1-i], sealHash[i]
+	}
+
+	fmt.Println("VerifySealFast(): contentHash=", hex.EncodeToString(contentHash[:]), "nonceExt=", nonce, "mixdigest=", hex.EncodeToString(mixDigest[:]), "seal hash=", hex.EncodeToString(sealHash[:]))
 }
