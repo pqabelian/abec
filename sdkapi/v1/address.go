e1876720144dc933257a3273da499860fd20f3f8
alicebobscorpio <alicebobscorpio@gmail.com>
2023-03-08 21:33:47 +0800

    Update address.go
    correct the comments.

--- sdkapi/v1/address.go
+++ sdkapi/v1/address.go
@@ -2,7 +2,7 @@ package v1
 
 import "github.com/abesuite/abec/abecrypto"
 
-// IsValidCryptoAddress checks wthether the given cryptoAddress is valid (in format).
+// CryptoAddressCheck checks wthether the given cryptoAddress is valid (in format).
 // When the result is false, the hints will give the reasons why it is invalid.
 func CryptoAddressCheck(cryptoAddress []byte) (bl bool, hints string) {
 	return abecrypto.CryptoAddressCheck(cryptoAddress)
