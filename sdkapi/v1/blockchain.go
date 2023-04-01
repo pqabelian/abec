c97329bb28cc5db3bddad6a4faca47e38c16a623
alicebobscorpio <alicebobscorpio@gmail.com>
2023-04-01 08:21:40 +0800

    initial API for tx gen and sign

--- sdkapi/v1/blockchain.go
+++ sdkapi/v1/blockchain.go
@@ -1,5 +1,1 @@
 package v1
-
-func BuildTxoRings(blockNumPerRingGroup int, txoRingSize int, serializedBlocks [][]byte) (serializedTxoRings [][]byte, err error) {
-	return nil, nil
-}
--- wire/msgtxabe_test.go
+++ wire/msgtxabe_test.go
@@ -19,3 +19,17 @@ func TestOutPointAbe_OutPointId(t *testing.T) {
 	fmt.Println(op.OutPointId())
 	fmt.Println(op.OutPointId())
 }
+
+func TestRingId(t *testing.T) {
+	randomByte := []byte("test")
+	hash := chainhash.DoubleHashH(randomByte)
+
+	ringMap := make(map[RingId]int)
+	ringMap[RingId(hash)] = 1
+
+	hash2 := chainhash.DoubleHashH(randomByte)
+	ringId := RingId(hash2)
+	if value, ok := ringMap[ringId]; ok {
+		fmt.Println(value)
+	}
+}
