927bde0616f06e023ee32395759ddd7bf213b438
depressi0n <793377875@qq.com>
2023-05-30 23:26:40 +0800

    Update message.go
    Expand the message size

--- wire/message.go
+++ wire/message.go
@@ -19,7 +19,7 @@ const CommandSize = 12
 
 // MaxMessagePayload is the maximum bytes a message can be regardless of other
 // individual limits imposed by messages themselves.
-const MaxMessagePayload = (1024 * 1024 * 32) // 32MB
+const MaxMessagePayload = (1024 * 1024 * 256) // 256MB
 
 // Commands used in bitcoin message headers which describe the type of message.
 const (
