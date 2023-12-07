8d8e6285d37158a5741c7ac46174ad2464e0284b
alicebobscorpio <alicebobscorpio@gmail.com>
2023-12-07 22:53:18 +0800

    prepare for TransferTxGen

--- abecryptox/abecrypto.go
+++ abecryptox/abecrypto.go
@@ -75,6 +75,8 @@ func CoinbaseTxGen(abeTxOutputDescs []*AbeTxOutputDesc, coinbaseTxMsgTemplate *w
 
 }
 
+// CoinbaseTxVerify verifies whether the input coinbaseTx *wire.MsgTxAbe is valid.
+// todo review
 func CoinbaseTxVerify(coinbaseTx *wire.MsgTxAbe) (bool, error) {
 	cryptoScheme, err := abecryptoxparam.GetCryptoSchemeByTxVersion(coinbaseTx.Version)
 	if err != nil {
@@ -91,8 +93,88 @@ func CoinbaseTxVerify(coinbaseTx *wire.MsgTxAbe) (bool, error) {
 	case abecryptoxparam.CryptoSchemePQRingCTX:
 		return pqringctxCoinbaseTxVerify(abecryptoxparam.PQRingCTXPP, coinbaseTx)
 	default:
-		return false, errors.New("CoinbaseTxVerify: Unsupported crypto scheme")
+		return false, fmt.Errorf("CoinbaseTxVerify: Unsupported crypto scheme")
 	}
+}
+
+// CreateTransferTxMsgTemplate creates a *wire.MsgTxAbe template, which will be used when calling TransferTxGen().
+// To be self-contained, we put it here, together with TransferTxGen().
+// The fields of *wire.MsgTxAbe template will be handled as below:
+// (1) Version: filled here
+// (2) TxIns: partially filled here, say filled except the serialNumber
+// (3) TxOuts: set empty here and will be filled by underlying crypto schemes
+// (4) TxFee: filled here
+// (5) TxMemo: filled here
+// (6) TxWitness: set empty here and will be filled by underlying crypto schemes
+// In summary, CreateTransferTxMsgTemplate() fills all fields except the serialNumber, TxOuts, and TxWitness.
+// Note that these filled fields (except the Version) are independent of the underlying crypto scheme,
+// and are specified by the issuer of a transaction.
+// todo: review
+func CreateTransferTxMsgTemplate(abeTxInputDescs []*AbeTxInputDesc, abeTxOutputDescs []*AbeTxOutputDesc, txFee uint64, txMemo []byte) (*wire.MsgTxAbe, error) {
+
+	//	Version
+	//	Note that new Tx must use the latest/current TxVersion.
+	txMsgTemplate := wire.NewMsgTxAbe(wire.TxVersion)
+
+	//	TxIns
+	for _, abeTxInputDesc := range abeTxInputDescs {
+		txIn := wire.NewTxInAbe(nil, abeTxInputDesc.txoRing.OutPointRing)
+		txMsgTemplate.AddTxIn(txIn)
+	}
+
+	//	TxOuts: skip
+
+	//	TxFee
+	txMsgTemplate.TxFee = txFee
+
+	//	TxMemo
+	txMsgTemplate.TxMemo = txMemo
+
+	//	TxWitness: skip
+
+	return txMsgTemplate, nil
+}
+
+func TransferTxGen(abeTxInputDescs []*AbeTxInputDesc, abeTxOutputDescs []*AbeTxOutputDesc, transferTxMsgTemplate *wire.MsgTxAbe) (*wire.MsgTxAbe, error) {
+
+	cryptoScheme, err := abecryptoxparam.GetCryptoSchemeByTxVersion(transferTxMsgTemplate.Version)
+	if err != nil {
+		return nil, err
+	}
+	//	Note that a transferTxMsgTemplate may be created by CreateTransferTxMsgTemplate in an early version, so that the transferTxMsgTemplate.Version is old version.
+
+	switch cryptoScheme {
+	case abecryptoxparam.CryptoSchemePQRingCT:
+		// This is to achieve back-compatibility with CryptoSchemePQRingCT.
+		// Before the BlockHeightMLP, the caller may use the transferTxMsgTemplate.Version that maps to CryptoSchemePQRingCT.
+		pqringctAbeTxInputDescs := make([]*abecrypto.AbeTxInputDesc, len(abeTxInputDescs))
+		pqringctAbeTxOutputDescs := make([]*abecrypto.AbeTxOutputDesc, len(abeTxOutputDescs))
+		for i := 0; i < len(abeTxInputDescs); i++ {
+			abeTxInputDesc := abeTxInputDescs[i]
+			pqringctAbeTxInputDescs[i] = abecrypto.NewAbeTxInputDescWithFullRing(abeTxInputDesc.ringId, abeTxInputDesc.txoRing, abeTxInputDesc.sidx,
+				abeTxInputDesc.cryptoAddress, abeTxInputDesc.cryptoSpsk, abeTxInputDesc.cryptoSnsk, abeTxInputDesc.cryptoVsk,
+				abeTxInputDesc.value)
+		}
+		for i := 0; i < len(abeTxOutputDescs); i++ {
+			pqringctAbeTxOutputDescs[i] = abecrypto.NewAbeTxOutDesc(abeTxOutputDescs[i].cryptoAddress, abeTxOutputDescs[i].value)
+		}
+
+		trTx, err := abecrypto.TransferTxGen(pqringctAbeTxInputDescs, pqringctAbeTxOutputDescs, transferTxMsgTemplate)
+		if err != nil {
+			return nil, err
+		}
+		return trTx, nil
+
+	case abecryptoxparam.CryptoSchemePQRingCTX:
+		trTx, err := pqringctxTransferTxGen(abecryptoxparam.PQRingCTXPP, cryptoScheme, abeTxInputDescs, abeTxOutputDescs, transferTxMsgTemplate)
+		if err != nil {
+			return nil, err
+		}
+		return trTx, nil
+	default:
+		return nil, errors.New("TransferTxGen: Unsupported crypto scheme")
+	}
+
 }
 
 func TransferTxVerify(transferTx *wire.MsgTxAbe, abeTxInDetails []*AbeTxInDetail) (bool, error) {
