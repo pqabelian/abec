d1ba2869878e9c3cdaac636c3291c70a0ff51d49
alicebobscorpio <alicebobscorpio@gmail.com>
2024-04-15 21:37:31 +0800

    Update crypto.go
    review

--- sdkapi/v2/crypto.go
+++ sdkapi/v2/crypto.go
@@ -3,7 +3,6 @@ package v2
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"github.com/abesuite/abec/abecryptox"
 	"github.com/abesuite/abec/abecryptox/abecryptoxkey"
 	"github.com/abesuite/abec/abecryptox/abecryptoxparam"
@@ -14,44 +13,19 @@ type CryptoScheme = abecryptoxparam.CryptoScheme
 type PrivacyLevel = abecryptoxkey.PrivacyLevel
 
 func GetCryptoSchemeByTxVersion(txVersion uint32) (CryptoScheme, error) {
-	cryptoScheme, err := abecryptoxparam.GetCryptoSchemeByTxVersion(txVersion)
-	if err != nil {
-		return 0, err
-	}
-	return CryptoScheme(cryptoScheme), nil
-}
-
-/*
-* CryptoAddressKeySeedGen generates a cryptoAddressKeySeed for current cryptoScheme, say PQRingCTX.
- */
-func CryptoAddressKeySeedGen(cryptoScheme CryptoScheme, privacyLevel PrivacyLevel) (coinSpendKeyRootSeed []byte,
-	coinSerialNumberKeyRootSeed []byte, coinValueKeyRootSeed []byte, coinDetectorRootKey []byte, err error) {
-	cryptoScheme = abecryptoxparam.GetCurrentCryptoScheme()
-	coinSpendKeyRootSeed, coinSerialNumberKeyRootSeed,
-		coinValueKeyRootSeed, coinDetectorRootKey, err = abecryptoxkey.CryptoAddressKeySeedGen(cryptoScheme, privacyLevel)
-	if err != nil {
-		return nil, nil, nil, nil, err
-	}
-
-	return coinSpendKeyRootSeed, coinSerialNumberKeyRootSeed, coinValueKeyRootSeed,
-		coinDetectorRootKey, nil
+	return abecryptoxparam.GetCryptoSchemeByTxVersion(txVersion)
 }
 
 func CryptoAddressKeyGenByRootSeeds(cryptoScheme CryptoScheme, privacyLevel PrivacyLevel,
 	coinSpendKeyRootSeed []byte, coinSerialNumberKeyRootSeed []byte, coinValueKeyRootSeed []byte,
 	coinDetectorRootKey []byte) (cryptoAddress []byte, cryptoSpsk []byte, cryptoSnsk []byte, cryptoVsk []byte, cryptoDetectorKey []byte, err error) {
-	cryptoAddress, cryptoSpsk, cryptoSnsk, cryptoVsk, cryptoDetectorKey, err = abecryptoxkey.CryptoAddressKeyGenByRootSeeds(cryptoScheme, privacyLevel,
+	return abecryptoxkey.CryptoAddressKeyGenByRootSeeds(cryptoScheme, privacyLevel,
 		coinSpendKeyRootSeed, coinSerialNumberKeyRootSeed,
 		coinValueKeyRootSeed, coinDetectorRootKey)
-	return cryptoAddress, cryptoSpsk, cryptoSnsk, cryptoVsk, cryptoDetectorKey, err
 }
 
-func CryptoAddressKeyVerify(cryptoAddress []byte, cryptoSpsk []byte, cryptoSnsk []byte, cryptoVsk []byte, cryptoDectectorKey []byte) (bool, error) {
-	valid, err := abecryptoxkey.CryptoAddressKeysVerify(cryptoAddress, cryptoSpsk, cryptoSnsk, cryptoVsk, cryptoDectectorKey)
-	if !valid {
-		return false, fmt.Errorf("CryptoAddressKeyVerify: fail to verify %s", err)
-	}
-	return true, nil
+func CryptoAddressKeyVerify(cryptoAddress []byte, cryptoSpsk []byte, cryptoSnsk []byte, cryptoVsk []byte, cryptoDetectorKey []byte) (bool, error) {
+	return abecryptoxkey.CryptoAddressKeysVerify(cryptoAddress, cryptoSpsk, cryptoSnsk, cryptoVsk, cryptoDetectorKey)
 }
 
 // CheckCryptoAddress checks whether the given cryptoAddress is valid (in format).
