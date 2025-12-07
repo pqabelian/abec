6951c08bb74e7ea23562cb5de7a70bc28b34740f
alicebobscorpio <alicebobscorpio@gmail.com>
2025-12-07 16:59:54 +0800

    refactor ctaut/param.go to ctaut/api/constants

---
+++ ctaut/api/constants.go
@@ -0,0 +1,20 @@
+package api
+
+import "github.com/abesuite/abec/ctaut/script"
+
+const MaxAmount = script.MaxAmount
+const InfiniteExpireHeight = script.InfiniteExpireHeight
+const MaxAutValueScriptLength = script.MaxAutValueScriptLength
+
+const MaxAutNameLength = script.MaxAutNameLength
+const MaxAutSymbolLength = script.MaxAutSymbolLength
+const MaxBaseUnitLength = script.MaxBaseUnitLength
+const MaxSubUnitLength = script.MaxSubUnitLength
+
+const (
+	AutScriptTypeRegistration   = script.AutScriptTypeRegistration
+	AutScriptTypeReRegistration = script.AutScriptTypeReRegistration
+	AutScriptTypeMint           = script.AutScriptTypeMint
+	AutScriptTypeTransfer       = script.AutScriptTypeTransfer
+	AutScriptTypeBurn           = script.AutScriptTypeBurn
+)
